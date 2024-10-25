09794b0649c1c3eec44140e03f242e0a74a1ae66
Eliam Vazquez <98252478+Beker06@users.noreply.github.com>
2024-10-24 23:11:09 -0700

    Test 2 import express

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import cors from "cors";  // Importar CORSimport express from "express";
+import express from "express";
+import cors from "cors"; 
 import dotenv from "dotenv"
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
@@ -14,7 +15,6 @@ connectDB()
 
 const PORT = process.env.PORT || 5000;
 
-
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
@@ -22,8 +22,8 @@ cloudinary.config({
 });
 
 app.use(cors({
-	origin: process.env.CLIENT_URL || "http://localhost:3000",  // Permitir solicitudes solo desde tu frontend
-	credentials: true,  // Permitir cookies y autenticación
+	origin: process.env.CLIENT_URL || "http://localhost:3000",
+	credentials: true,
 }));
 
 app.use(express.json({ limit: "50mb" }));
@@ -34,6 +34,7 @@ app.use(cookieParser())
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
+
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
